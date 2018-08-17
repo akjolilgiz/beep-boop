b758a0bdc0aae473f4067365ffed547df0802df4
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-08-17 13:32:58 -0700

    add function to return Boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,20 @@
 
   var beepConverter = function(number){
-    var indNumbers = [];
-    var digits = number.toString();
-
-    for (var i = 0, i <digit.length, i += 1) {
-      indNumbers.push(+digits.charAt(i));
-  }
-      // result.push(+digits.charAt());
-    }
-    });
-
+   var digits = number.toString();
 
      if (isNaN(number)) {
       return output = 1;
     }
-        else if (number % 30 === 0) {
-          return output=2;
+        else if (number % 3 === 0) {
+          return output = 2;
       }
-        else if (one.includes(number(i))) {
-          alert(digits)
+        else if (digits.includes(0)) {
+          return output=3;
                 }
-        else {
-        alert("Beep");
-      }
-    }
-
+    //     else if (digits.includes(1)){
+    //       return output = 4}
+    // }
+    //
 
 
 
@@ -39,9 +29,13 @@ $(document).ready(function() {
     if (output === 1) {
       output = "Please Enter A Number"
     } else if (output === 2){
-      output = "I'm sorry, Dave. I'm afraid I can't do that."}
-    // } else if
-
+      output = "I'm sorry, Dave. I'm afraid I can't do that."
+      } else if (output === 3){
+        output = "Boop"
+      } else if (output === 4){
+          output = "Beep"
+        }
+          else {}
     $("#result").text(output);
   });
 });
