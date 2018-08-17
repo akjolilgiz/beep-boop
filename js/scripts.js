34766ccc31a4e67d5f842b440a0627c25534e67e
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-08-17 11:10:19 -0700

    Add function to return I'm Sorry Dave

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@
      if (isNaN(number)) {
       return output = 1;
     }
-      //   else if (number % 30 === 0) {
-      //     return output=2;
-      // }
+        else if (number % 30 === 0) {
+          return output=2;
+      }
 
         else {
         alert("Beep");
@@ -27,7 +27,7 @@ $(document).ready(function() {
       output = "Please Enter A Number"
     } else if (output === 2){
       output = "I'm sorry, Dave. I'm afraid I can't do that."
-    }
+    } else if
     $("#result").text(output);
   });
 });
